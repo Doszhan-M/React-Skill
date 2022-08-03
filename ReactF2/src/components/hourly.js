2647b5b42af15b6811747551c7401f19c3553c6c
Doszhan <doszhan.m@mail.ru>
2022-08-03 23:04:44 +0600

    2 row howrly

--- ReactF2/src/components/hourly.js
+++ ReactF2/src/components/hourly.js
@@ -7,16 +7,26 @@ import ScrollContainer from 'react-indiana-drag-scroll'
 
 function Hourly(props) {
 
-    let [hourly, setHourly] = useState([])
+    let [firstDayHourly, setFirstDayHourly] = useState([])
+    let [secondDayHourly, setSecondDayHourly] = useState([])
 
     useEffect(
         () => {
             if (props.weatherForecast) {
-                setHourly(props.weatherForecast.hourly)
+                let firstDay = [];
+                props.weatherForecast.hourly.map((hour, index) => {
+                    if (index < 25) {firstDay.push(hour)}
+                })
+                setFirstDayHourly(firstDay)
+
+                let secondDay = [];
+                props.weatherForecast.hourly.map((hour, index) => {
+                    if (index > 24) {secondDay.push(hour)}
+                })
+                setSecondDayHourly(secondDay)
             }
         }, [props.weatherForecast]
     )
-
     const container = useRef(null);
     useEffect(() => {
         container.current.getElement().scrollTo(0, Math.random() * 10000);
@@ -30,13 +40,19 @@ function Hourly(props) {
             props.setCurrentIndex(index)
         }
     }
-    let isDark = props.isDark 
+    let isDark = props.isDark
     return (
         <div className="hourly">
-            <div className="hourly_title">Hourly</div>
+            <div className="hourly_title">Today</div>
             <div className="hourly_items_line">
                 <ScrollContainer className="container flex_container" ref={container}>
-                    {hourly.map((hour, index) => <Hour key={hour.dt} {...{ hour, index, onClick, isDark}} />)}
+                    {firstDayHourly.map((hour, index) => <Hour key={hour.dt} {...{ hour, index, onClick, isDark }} />)}
+                </ScrollContainer>
+            </div>
+            <div className="hourly_title">Tomorrow</div>
+            <div className="hourly_items_line">
+                <ScrollContainer className="container flex_container" ref={container}>
+                    {secondDayHourly.map((hour, index) => <Hour key={hour.dt} {...{ hour, index, onClick, isDark }} />)}
                 </ScrollContainer>
             </div>
         </div>
